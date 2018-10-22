a3e6f55b15f3e347f6343ef9fc5a7d8f61159c7b
Zihan Zhang <769492047@qq.com>
2018-10-22 22:33:55 +0100

    Get courses endpoint tested

--- test/routes/courses-test.js
+++ test/routes/courses-test.js
@@ -1,4 +1,4 @@
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = 'development';
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../../bin/www');
@@ -9,5 +9,25 @@ let _ = require('lodash' );
 chai.use(require('chai-things'));
 
 describe('Courses', function (){
-   
+    describe('GET /courses',  () => {
+        it('should return all the courses in an array', function(done) {
+            chai.request(server)
+                .get('/courses')
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('array');
+                    expect(res.body.length).to.equal(4);
+                    let result = _.map(res.body, (course) => {
+                        return { name: course.name,
+                            type: course.type }
+                    });
+                    expect(result).to.include( { name: "math", type: "P"  } );
+                    // expect(result).to.include( { name: "english", type: "P"  } );
+
+                    done();
+                });
+
+        });
+    });
+    
 });
